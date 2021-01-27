80cd21e0500fdb1a5a729163a845e501d2a68812
shekhar-kadam <52596546+shekhar-kadam@users.noreply.github.com>
2021-01-27 23:27:43 +0530

    Add files via upload
    Day 4 assignment on time..

---
+++ Day4/Day4.js
@@ -0,0 +1,30 @@
+// //By how many ways we can access dom elements in the DOM and write about them in brief?
+
+// There are majorily three ways to access th dom elements which are
+
+// first is we can access by the id of the html tag
+
+// document.getElementById(id)
+
+// where we can find the element by there uniquue id.There
+
+// second is we can access by the tag of the html tag
+
+// document.getElementsByTagName(name)
+
+// where we can access the element by their tag names and we can select all the same tag in one time
+
+// Third is we can access by the class of the html tag that we provide them
+
+// document.getElementsByClassName(name)
+
+// where we can access the element by there unique class name.
+
+
+
+const addTwoNumbers = () => {
+    let a = document.getElementById("firstNumber").value;
+    let b = document.getElementById("secondNumber").value;
+    console.log( c = parseInt(a) + parseInt(b))
+    
+}
